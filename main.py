b9d379fc8cbf2c8da036160217a0c2fb5705b461
Yifan Huang <709703227@qq.com>
2024-06-15 22:00:58 +0800

    Verify if the main.py is running properly.

--- main.py
+++ main.py
@@ -78,12 +78,12 @@ lit_module = ModelLightningModule(model=model)
 logger = CSVLogger(".", name="logs")
 checkpoint_callback = ModelCheckpoint(monitor='val_Total_Loss', save_last=True)
 # early_stopping = EarlyStopping(monitor='val_Total_Loss', min_delta=0.0, patience=3, mode='min')
-trainer = pl.Trainer(max_epochs=10,
+trainer = pl.Trainer(max_epochs=5,
                      accelerator="cpu",
                      logger=logger,
                      callbacks=[checkpoint_callback])
 trainer.fit(model=lit_module, train_dataloaders=train_loader, val_dataloaders=val_loader)
 
-# # prediction
-# pred_res = trainer.predict(model=lit_module, dataloaders=test_loader)
-# print(pred_res)
+# prediction
+pred_res = trainer.predict(model=lit_module, dataloaders=test_loader)
+print(pred_res)
